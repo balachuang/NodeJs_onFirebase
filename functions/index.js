087add1a576dc1baaac094f45f6e9dd0eaff3d49
balachuang <balachuang@gmail.com>
2024-10-03 22:01:45 +0800

    update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,10 @@
 const functions = require('firebase-functions');
 const express = require('express');
-//const logger = functions.logger;
 
 const app = express();
-app.get('/timestamp', (request, response) => {
+app.get('/time', (request, response) => {
     let dateStr = (new Date(Date.now())).toUTCString();
-    response.send(dateStr);
+    response.send("Current time: " + dateStr);
 });
 
 exports.app = functions.https.onRequest(app);
